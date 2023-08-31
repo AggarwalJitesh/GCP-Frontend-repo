d62b3e4e8fbcdc1d93f60390c71f101f79bebe7d
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2023-08-31 21:03:09 +0530

    dashboard correction

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -1,9 +1,7 @@
 import Rechart from "../inc/rechart";
-import { Link, useLocation } from "react-router-dom";
+import { Link} from "react-router-dom";
 import Navigation from "../inc/navigation";
-function Dashboard(props) {
-  const { state } = useLocation();
-  const { username } = state;
+function Dashboard() {
   return (
     <>
       <div class="banner1">
@@ -11,9 +9,6 @@ function Dashboard(props) {
         <div class="d-flex justify-content-center">
           <Rechart />
         </div>
-      </div>
-      <div>
-        <p>username: {username}</p>
       </div>
     </>
   );
