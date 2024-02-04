ca2b2cdc3857ca662f21b78fcc28a69fc1791ed3
Jitesh Aggarwal <96955945+AggarwalJitesh@users.noreply.github.com>
2024-02-04 12:11:51 +0530

    minor changes

--- src/components/pages/dashboard.js
+++ src/components/pages/dashboard.js
@@ -4,13 +4,14 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Dashboard = ({ userName }) => {
-  const [transaction, setTransaction] = useState({});
+  const [data, setData] = useState([]);
+
   useEffect(() => {
     // Adjust the fetch URL to match your FastAPI endpoint
     fetch("http://127.0.0.1:8000/transaction/")
       .then((response) => response.json())
       .then((data) => {
-        setTransaction(data); // Set data in an array to map over it
+        setData([data]); // Set the fetched data into the state
       });
   }, []);
 
@@ -24,15 +25,29 @@ const Dashboard = ({ userName }) => {
       <table className="table">
         <thead>
           <tr>
-            <th>Field</th>
-            <th>Value</th>
+            <th>Block Hash</th>
+            <th>Block Number</th>
+            <th>Hash</th>
+            <th>Image Name</th>
+            <th>Result</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {Object.entries(transaction).map(([key, value]) => (
-            <tr key={key}>
-              <td>{key}</td>
-              <td>{value}</td>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.blockHash}</td>
+              <td>{item.blockNumber}</td>
+              <td>{item.hash}</td>
+              {/* <td>{item.imageName}</td>
+              <td>{item.result}</td> */}
+              <td>"imageName"</td>
+              <td>"result"</td>
+              <td>
+                <a href="https://storage.cloud.google.com/demo_blockconvey/static/image_01ae8ea8-f7c4-4fdd-8a14-3cceb2c902fa.jpg">
+                  View Image
+                </a>
+              </td>
             </tr>
           ))}
         </tbody>
